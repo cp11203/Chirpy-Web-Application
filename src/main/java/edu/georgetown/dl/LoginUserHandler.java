24f69bdb95843c60f34fc5b9b61b7409fa6d9841
bugurdag <122176951+bugurdag@users.noreply.github.com>
2024-02-27 00:58:49 -0500

    Update LoginUserHandler.java

--- src/main/java/edu/georgetown/dl/LoginUserHandler.java
+++ src/main/java/edu/georgetown/dl/LoginUserHandler.java
@@ -13,6 +13,8 @@ import edu.georgetown.bll.user.UserService;
 public class LoginUserHandler implements HttpHandler {
 
     final String FORM_PAGE = "login.thtml";
+    final String MAIN_PAGE = "main.thtml";
+    
     private Logger logger;
     private DisplayLogic displayLogic;
     private UserService userService;
@@ -37,9 +39,15 @@ public class LoginUserHandler implements HttpHandler {
             String password = dataFromWebForm.get("password");
 
             // Use the userService to log the login attempt
-            userService.loginUser(username, password);
-
-            // NEED TO HANDLE ERRORS
+            boolean loginSuccess = userService.loginUser(username, password);
+            
+            if (loginSuccess) {
+                exchange.getResponseHeaders().set("Location", MAIN_PAGE); //redir to main
+                exchange.sendResponseHeaders(302, -1);
+                return; // End the request after redirect
+            } else {
+                dataModel.put("error", "Invalid username or password");
+            }
         } 
         StringWriter sw = new StringWriter();
 
